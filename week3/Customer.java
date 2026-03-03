1934aa560de81f904e0ecf3560b4c127d422716f
Mathe Ervin <ervin.mathe79@gmail.com>
2026-03-03 16:51:08 +0200

    Java Lab 3 class

---
+++ week3/Customer.java
@@ -0,0 +1,69 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import oop.labor02.BankAccount;
+
+public class Customer {
+    private String firstName ;
+    private String lastName ; 
+    static final int MAX_ACCOUNT = 10 ;
+    int numberOfAccounts = 0 ;
+    List<BankAccount> accounts = new ArrayList<>() ;
+
+
+    public Customer(String firtName , String lastName , BankAccount account) {
+        this.firstName = firtName ;
+        this.lastName = lastName ;
+        addAccount(account);
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public BankAccount getAccount(String accountNumber) {
+        for(BankAccount account : accounts) {
+            if(account.getAccountNumber().equals(accountNumber)) {
+                return account ;
+            }
+        }
+        return null ;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Override
+    public String toString() {
+        return "" + firstName + " " + lastName + " " + accounts.stream().map(a -> a.currentState()).reduce("", (a, b) -> a + "\n" + b) ;
+    }
+
+    public void addAccount(oop.labor02.BankAccount account) {
+        if(account == null) {
+            return ;
+        }
+        if(numberOfAccounts >= MAX_ACCOUNT) {
+            return ;
+        } else {
+            accounts.add(account) ;
+            numberOfAccounts++ ;
+        }
+        
+    }
+
+
+    public void closeAccount(String accountNumber) {
+        BankAccount account = getAccount(accountNumber) ;
+        if(account == null) {
+            System.out.println("Account not found") ;
+            return ;
+        }
+        account.closeAccount();
+        numberOfAccounts-- ;
+    }
+
+
+}
---
+++ week3/Lab3.java
@@ -0,0 +1,46 @@
+import oop.labor02.BankAccount;
+import Customer ;
+public class Lab3 {
+
+
+    public static void main(String[] args) {
+        //oop.labor02.BankAccount acc1 = new oop.labor02.BankAccount("001") ;
+        //oop.labor02.BankAccount acc2 = new oop.labor02.BankAccount("002" , 2000) ;
+
+
+        //System.out.println(acc1.currentState());
+        //System.out.println(acc2.currentState());
+
+        // Customer c1 = new Customer("Mathe" , "Ervin" , acc1) ;
+        // System.out.println(c1);
+        // System.out.println(c1);
+
+        // c1.getAccount().deposit(2000);
+
+
+
+
+
+        // oop.labor02.BankAccount acc2 = new oop.labor02.BankAccount("BT002" , 0) ;
+        // Customer c2 = new Customer("Marry" , "White" , acc2) ;
+        // System.out.println(c2);
+
+        // c2.getAccount().deposit(5000);
+        // c2.getAccount().closeAccount();
+        // System.out.println(c2);
+
+        // c2.setAccount(acc1); 
+
+        // System.out.println("\n" + c1);
+        // System.out.println(c2);
+
+
+        Customer c1 = new Customer("Mathe" , "Ervin" , null) ;
+        BankAccount acc1 = new BankAccount("OTP001") ;
+        BankAccount acc2 = new BankAccount("BT002" , 2000) ;
+        c1.addAccount(acc1);
+        c1.addAccount(acc2);
+        System.out.println(c1);
+
+    }
+}
